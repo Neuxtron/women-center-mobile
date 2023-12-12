c3d05f7d4c117b8cd4191fb619df1717fa3f4f07
Neuxtron <71965568+Neuxtron@users.noreply.github.com>
2023-12-12 12:04:19 +0700

    Revert "Merge branch 'main' of https://github.com/Neuxtron/women-center-mobile1"
    This reverts commit 888e77382f6d187e86f22a9b0381a09e50a55c97, reversing
    changes made to aae9925acb8ad300c9c530de11123ba51634f9d3.

--- lib/View/homepage/homepage_view.dart
+++ lib/View/homepage/homepage_view.dart
@@ -9,10 +9,8 @@ import 'package:women_center_mobile/ViewModel/artikel_view_model/artikel_view_mo
 import 'package:women_center_mobile/ViewModel/career_view_model/career_view_model.dart';
 
 class HomepageSection extends StatefulWidget {
-  const HomepageSection({super.key});
-
   @override
-  State<HomepageSection> createState() => _HomepageSectionState();
+  _HomepageSectionState createState() => _HomepageSectionState();
 }
 
 class _HomepageSectionState extends State<HomepageSection> {
